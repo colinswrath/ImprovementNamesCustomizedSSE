1276c24f76693872c8807069fd17d39cce3d91c6
colinswrath <colinkelley30@gmail.com>
2021-12-02 13:01:39 -0500

    Removed build files

--- build/CommonLibSSE/CommonLibSSEConfig.cmake
+++
@@ -1,6 +0,0 @@
-include("${CMAKE_CURRENT_LIST_DIR}/CommonLibSSE-targets.cmake")
-include(CMakeFindDependencyMacro)
-
-find_dependency(binary_io CONFIG)
-find_dependency(Boost MODULE)
-find_dependency(spdlog CONFIG)
